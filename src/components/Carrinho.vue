2473ed27edb42abf912af00f1173b5b91bc23121
Lucas Salles <lucas.sarantes@gmail.com>
2023-03-24 22:26:10 -0300

    Funcionalidade de trocar o estado do carrinho de acordo com a quantidade de itens no array

--- src/components/Carrinho.vue
+++ src/components/Carrinho.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-1 absolute top-[6rem] right-[0rem] shadow-lg w-8/12 md:w-3/12 bg-white pb-5">
         <p class="w-full font-bold border-2 border-b-green-800 text-left p-5">Carrinho</p>
-        <div v-show="mostrar">
+        <div v-show="!produtosNoCarrinho.value">
             <p class="text-center pt-5">Não há itens no carrinho.</p>
         </div>
     </div>
@@ -10,19 +10,22 @@
 <script setup>
 import { useProdutosStore } from '@/store';
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
 let mostrar = true;
-let produtosNoCarrinho = [];
+let produtosNoCarrinho = ref([]);
 const store = useProdutosStore();
 const { produtos } = storeToRefs(store);
 //console.log(produtos);
+
+
 produtos.value.forEach(produto => {
     if (produto.quantidade > 0) {
         if (mostrar === true) {
             mostrar = false;
         }
-        produtosNoCarrinho.push(produto);
-        console.log(produtosNoCarrinho);
+        produtosNoCarrinho.value.push(produto);
+        console.log(produtosNoCarrinho.value);
     }
 });
 
